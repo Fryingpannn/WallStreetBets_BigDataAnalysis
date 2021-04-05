acb7536eef8b3dc9dcd198ef2096778d55842e5e
mlz5080 <mlz961116@gmail.com>
2021-04-05 00:50:47 -0400

    add basic clustering

--- naive_bayes_analysis.py
+++ naive_bayes_analysis.py
@@ -5,6 +5,8 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import split as splitsp
 from pyspark.ml.feature import CountVectorizer
 from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
 
 if __name__ == "__main__":
     spark = SparkSession\
@@ -15,7 +17,7 @@ if __name__ == "__main__":
 
     # $example on$
     # Load training data
-    df = spark.read.csv("Data/7days-updated-wsbdata.csv",header='true')
+    df = spark.read.csv("Data/updated-30dayswsb.csv",header='true')
     _df = df.withColumn(colName="text_vector",col = splitsp(df.text," "))
 
     #Removing stop words
@@ -27,16 +29,24 @@ if __name__ == "__main__":
     #CountVecotrizing with filtered DF
     cv = CountVectorizer(inputCol="filtered",outputCol="features")
     model = cv.fit(filtered_df)
-    print(len(model.vocabulary))
+    #print(model.vocabulary)
     result = model.transform(filtered_df)
 
-    print(result.first().features.toArray())
-    #print(type(result.features))
+    kmeans = KMeans().setK(3).setSeed(2)
+    model = kmeans.fit(result.select("features"))
 
+    predictions = model.transform(result.select("features"))
+    evaluator = ClusteringEvaluator()
+    silhouette = evaluator.evaluate(predictions)
 
-    #result1 = transform(result)
+    # Shows the result.
+    centers = model.clusterCenters()
 
-    #result2 = transform(result)
+    #probably model.data? [[0,10,20],[30,40,60]......]
+    # for : for: print vol[0],vol[10]
+    print("Cluster Centers: ")
+    for center in centers:
+        print(center)
 
 
     #count_vectorizer = CountVectorizer(inputCol=_df.text_vector,outputCol="features",stop_words="English")
